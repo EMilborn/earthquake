8a818bfc88d9b5b7fbcff46caeb1b0561d25056e
ThisIsMyNick <nobel.gautam@gmail.com>
2017-01-19 12:11:36 -0500

    code for avg latency in past N seconds

--- utils/lagcomp.py
+++ utils/lagcomp.py
@@ -6,15 +6,32 @@ from copy import deepcopy
 class LagCompClass:
     def __init__(self):
         self.history = deque()
+        self.latency = deque()
+
+
     def add_state(self, pos, mouse):
         self.history.append((time.time(), deepcopy(pos), deepcopy(mouse)))
+
+
     def remove_old_states(self):
         now = time.time()
         while len(self.history) > 0 and (now - self.history[0][0]) > 1:
             self.history.popleft()
+
+
     def get_approx_client_state(self):
         now = time.time()
-        lag = 0.05 #Placeholder. Need to find latency from socketio
-        then = now - lag
+        then = now - self.get_avg_latency()
         state = min(self.history, key = lambda x: abs(x[0]-then))
         return state
+
+    def add_latency(self, latency):
+        now = time.time()
+        while len(self.latency) > 0 and (now - self.latency[0][0]) > 2:
+            self.latency.popleft()
+        self.latency.append(latency)
+
+
+    def get_avg_latency(self):
+        return float(sum(self.latency))/len(self.latency)
+
