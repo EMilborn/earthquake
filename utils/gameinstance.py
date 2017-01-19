a8bae1ed0b1b6c4003d09c0bca207b605d058bb7
71619997a <marksg07@gmail.com>
2017-01-19 12:27:29 -0500

    Split game.py

---
+++ utils/gameinstance.py
@@ -0,0 +1,102 @@
+from player import Player
+from bullet import Bullet
+
+TICKRATE_DEFAULT = 120  # the tickrate that the following numbers are based on
+TICKRATE = 125  # real tickrate, in case we change
+TICKMULT = 1.0 * TICKRATE_DEFAULT / TICKRATE
+
+PLAYER_HEALTH = 100
+PLAYER_SPEED = 3 * TICKMULT
+PLAYER_RADIUS = 25
+
+BULLET_DAMAGE = 10
+BULLET_RADIUS = 5
+BULLET_DELAY = 30 * TICKMULT
+BULLET_SPEED = 10 * TICKMULT
+
+
+class Instance:
+
+    def __init__(self, user1, user2):
+        self.players = {}
+        self.scores = {}
+        self.bullets = []
+        self.addUser(user1)
+        self.addUser(user2)
+
+    def addUser(self, uid):
+        print 'adding user to game'
+        self.players[uid] = Player(uid)
+        self.scores[uid] = 0
+
+    def handleEvent(self, event):
+        etype = event['event']
+        uid = event['user']
+        user = self.players[uid]
+        if etype == 'key':
+            key = event['key']
+            keyDown = event['state']
+            if key == 'LeftArrow':
+                user.input.left = keyDown
+            elif key == 'RightArrow':
+                user.input.right = keyDown
+            elif key == 'UpArrow':
+                user.input.up = keyDown
+            elif key == 'DownArrow':
+                user.input.down = keyDown
+            elif key == 'Mouse1':
+                user.input.mouse1 = keyDown
+        if etype == 'mousemove':
+            if user.input.mousePos is None:
+                user.input.mousePos = Vector(0, 0)
+            user.input.mousePos.x = event['x']
+            user.input.mousePos.y = event['y']
+
+    def endGame(self):
+        pass
+
+    def gameLoop(self):
+        for uid, user in self.players.iteritems():
+            if user.health <= 0:
+                self.endgame()
+            if user.input.left:
+                user.pos.x -= 1
+            if user.input.right:
+                user.pos.x += 1
+            if user.input.up:
+                user.pos.y -= 1
+            if user.input.down:
+                user.pos.y += 1
+            user.cooldown -= 1
+            if user.input.mouse1 and user.cooldown < 0:
+                print 'adding a bullet'
+                client_state = user.lagcomp.get_approx_client_state()
+                print "bt time:", client_state[0]
+                pos = client_state[1]
+                mousePos = client_state[2]
+                if mousePos:
+                    bulletVel = (mousePos-pos).normalized() * BULLET_SPEED
+                    newBullet = Bullet(uid, pos, bulletVel)
+                    self.bullets.append(newBullet)
+                    user.cooldown = BULLET_DELAY
+
+            user.lagcomp.remove_old_states()
+            user.lagcomp.add_state(user.pos, user.input.mousePos)
+
+        for bullet in self.bullets:
+            bullet.update()
+            for id, user in self.players.iteritems():
+                if bullet.collides(user):
+                    self.bullets.remove(bullet)
+
+            # if user.input.click:
+            # self.bullets.append(bullet(id, user.x, user.y,
+
+    def getGameState(self):
+        data = {'users': [], 'bullets': []}
+        for uid, player in self.players.iteritems():
+            data['users'].append({'x': player.pos.x, 'y': player.pos.y})
+        for bullet in self.bullets:
+            data['bullets'].append({'x': bullet.pos.x, 'y': bullet.pos.y})
+        # print usercoords
+        return data
