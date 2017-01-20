cac062565d664785e90e4189f50b0b20e7128512
ThisIsMyNick <nobel.gautam@gmail.com>
2017-01-20 12:15:24 -0500

    more lag comp

--- utils/lagcomp.py
+++ utils/lagcomp.py
@@ -9,8 +9,8 @@ class LagCompClass:
         self.latency = deque()
 
 
-    def add_state(self, pos, mouse):
-        self.history.append((time.time(), deepcopy(pos), deepcopy(mouse)))
+    def add_state(self, pos):
+        self.history.append((time.time(), deepcopy(pos)))
 
 
     def remove_old_states(self):
@@ -20,8 +20,10 @@ class LagCompClass:
 
 
     def get_approx_client_state(self):
+        if len(self.history) == 0:
+            return -1
         now = time.time()
-        latency = self.get_avg_latency()
+        latency = min(0.2, self.get_avg_latency())
         then = now - latency/2
         print "LATENCY", latency
         state = min(self.history, key = lambda x: abs(x[0]-then))
@@ -37,5 +39,4 @@ class LagCompClass:
     def get_avg_latency(self):
         if len(self.latency) == 0:
             return 0
-        return float(sum(self.latency))/len(self.latency)
-
+        return float(sum(map(lambda x:x[1], self.latency)))/len(self.latency)
