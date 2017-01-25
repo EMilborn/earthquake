e6834c7f6df2c9c472d0acdbaa586751a3d67c13
ThisIsMyNick <nobel.gautam@gmail.com>
2017-01-25 14:44:21 -0500

    add map stuff

--- utils/map.py
+++ utils/map.py
@@ -1,11 +1,18 @@
+import os
+import glob
+import random
+
 class MapTiles:
     WALL = "WALL"
     EMPTY = "EMPTY"
     SPAWN = "SPAWN"
+    ERROR = "ERROR"
 
 class Map:
-    def __init__(self):
+    def __init__(self, fn=""):
         self.layout = [[]]
+        if fn != "":
+            self.loadmap(fn)
     def loadmap(self, fn):
         with open(fn, "r") as f:
             map_lines = f.readlines()
@@ -23,12 +30,31 @@ class Map:
             return MapTiles.EMPTY
         if c == 'o':
             return MapTiles.SPAWN
+        else:
+            return MapTiles.ERROR
+
+class MapPicker:
+    def __init__(self):
+        self.maps = []
+        for f in glob.glob("maps/*.map"):
+            print f
+            self.maps.append(f)
+    def get_maps(self):
+        return self.maps
+    def rand_map(self):
+        return random.choice(self.maps)
+
 
 """
 if __name__ == '__main__':
-    mp = Map()
-    mp.loadmap("test.map")
+    mpick = MapPicker()
+    print mpick.get_maps()
+    print mpick.rand_map()
+    mp = Map(mpick.rand_map())
+    #mp.loadmap(mpick.rand_map())
+    print mp.get()
+    print mp.get_at(0,0)
+    print mp.get_at(0,1)
     print mp.get_at(1,1)
-    print mp.get_at(1,2)
     mp.display()
 """
